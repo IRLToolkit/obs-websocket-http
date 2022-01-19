3744ccc74b05a304ef23220f3dc5c3dd7f44bb77
tt2468 <tt2468@gmail.com>
2022-01-18 21:54:17 -0800

    Full rewrite for v5, with many improvements

--- README.md
+++ README.md
@@ -9,32 +9,33 @@ Click [here](https://github.com/IRLToolkit/obs-websocket-http/tree/old-4.x) to g
 ## Installing on Ubuntu:
 - Clone/download the repository
 - Edit `config.ini` and set the address, port, and authentication details for the HTTP server (leave `authentication_key` empty for no auth). Set your obs-websocket connection settings in the `[obsws]` section.
-- `sudo apt update && sudo apt install python3.7 python3-pip`
-- `python3.7 -m pip install -r requirements.txt`
+- `sudo apt update && sudo apt install python3.8 python3-pip`
+- `python3.8 -m pip install -r requirements.txt`
 - CD into the `obs-websocket-http` directory
-- Run with `python3.7 main.py`
+- Run with `python3.8 main.py`
+
+Use `python3.8 main.py --help` to see command line options, which allow you to run this script without a config.ini.
 
 ## Running with Docker
-
 - Clone/download the repository
 - Edit `docker-compose.yml` to have the correct IPs and ports for this machine and the one running OBS Studio (it may be the same machine). You do NOT need to edit `config.ini` if using docker because it will be created by the container from the values in `docker-compose.yml`.
 - Start obs-websocket-http by running `docker-compose up -d && docker-compose logs -f`. This will give you log output and you can press `Ctrl-C` when you wish to return to terminal and the container will run in the background.
 
 ## Protocol:
-This code contains two request endpoints. `/emit/{requesttype}` and `/call/{requesttype}`.
-- `/emit/{requesttype}` sends off a websocket event without waiting for a response, and immediately returns a generic `{"status":"ok"}` json response after sending the event, regardless of whether it errors out on the OBS instance.
-- `/call/{requesttype}` Makes a full request to obs-websocket, and waits for a response. The recieved response is then returned to the HTTP caller.
+The web server contains these endpoints:
+- `/emit/{requestType}` sends off a websocket event without waiting for a response, and immediately returns a generic `{"result": true}` JSON response without a request result.
+- `/call/{requestType}` Makes a full request to obs-websocket, and waits for a response. The recieved response is then returned to the HTTP caller.
+  - Example JSON response: `{"result": true, "requestResult": {"requestType": "GetCurrentProgramScene", "requestStatus": {"result": true, "code": 100}, "responseData": {"currentProgramSceneName": "Scene 5"}}}`
 
-If authentication is set, then each request much contain an `AuthKey` header with the configured password as the value.
+If authentication is set, then each request much contain an `Authorization` header with the configured auth key as the value.
 
-A request type is always required, however a json body depends on the underlying request in obs-websocket as to whether any data is necessary.
+A request type is always required, however the request body is optional, and is forwarded as the request data for the obs-websocket request. If your obs-websocket request does not require request data, then no body is needed.
 
-For a list of request types, refer to the [obs-websocket protocol docs](https://github.com/Palakis/obs-websocket/blob/4.x-current/docs/generated/protocol.md#requests)
+For a list of request types, refer to the [obs-websocket protocol docs](https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#requests)
 
 ## Example cURL commands:
-- `curl -XPOST -H "Content-type: application/json" -d '{"scene-name":"Scene 2"}' 'http://127.0.0.1:4445/emit/SetCurrentScene'`
-- `curl -XPOST -H 'AuthKey: agoodpassword' -H "Content-type: application/json" -d '{"scene-name":"Scene 2"}' 'http://127.0.0.1:4445/emit/SetCurrentScene'`
-- `curl -XPOST -H 'AuthKey: agoodpassword' -H "Content-type: application/json" 'http://127.0.0.1:4445/call/GetSceneList'`
+- `curl -XPOST -H 'Authorization: pp123' -H "Content-type: application/json" -d '{"sceneName": "Scene 5"}' 'http://127.0.0.1:4445/emit/SetCurrentProgramScene'`
+- `curl -XPOST -H "Content-type: application/json" 'http://127.0.0.1:4445/call/GetCurrentProgramScene'`
 
 ## IRLTookit Links
 
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import logging
+logging.basicConfig(level=logging.INFO)
 import os
+import argparse
 import asyncio
 import json
 import simpleobsws
@@ -6,24 +9,31 @@ import aiohttp
 from aiohttp import web
 from configparser import ConfigParser
 
-config = ConfigParser()
-config.read('config.ini')
-httpAddress = config.get('http', 'bind_to_address')
-httpPort = config.getint('http', 'bind_to_port')
-httpAuthKey = config.get('http', 'authentication_key')
-if httpAuthKey:
-    print('Starting HTTP server with AuthKey set to "{}"'.format(httpAuthKey))
-else:
-    print('Starting HTTP server without authentication.')
-    httpAuthKey = None
-wsAddress = config.get('obsws', 'ws_address')
-wsPort = config.getint('obsws', 'ws_port')
-wsPassword = config.get('obsws', 'ws_password')
 loop = asyncio.get_event_loop()
-ws = simpleobsws.WebSocketClient(url='ws://{}:{}'.format(wsAddress, wsPort), password=wsPassword)
+app = web.Application()
+ws = None
 
-def statusmessage(message):
-    print(str(message) + '...      ', end='', flush=True)
+# Make aiohttp shut up
+aiohttpLogger = logging.getLogger('aiohttp')
+aiohttpLogger.setLevel(logging.WARNING)
+
+def fail_response(comment):
+    return web.json_response({'result': False, 'comment': comment})
+
+def validate_request(request):
+    if not httpAuthKey:
+        return True, None
+    if 'Authorization' not in request.headers:
+        return False, 'You are missing the `Authorization` header.'
+    if request.headers['Authorization'] != httpAuthKey:
+        return False, 'Invalid authorization key.'
+    return True, None
+
+async def get_json(request):
+    try:
+        return await request.json()
+    except json.decoder.JSONDecodeError:
+        return None
 
 def response_to_object(response: simpleobsws.RequestResponse):
     ret = {}
@@ -31,61 +41,92 @@ def response_to_object(response: simpleobsws.RequestResponse):
     ret['requestStatus'] = {'result': response.requestStatus.result, 'code': response.requestStatus.code}
     if response.requestStatus.comment:
         ret['requestStatus']['comment'] = response.requestStatus.comment
-    if ret.has_data():
+    if response.responseData:
         ret['responseData'] = response.responseData
     return ret
 
-async def handle_emit_request(request):
-    """Handler function for all emit-based HTTP requests. Assumes that you know what you are doing because it will never return an error."""
-    if ('AuthKey' not in request.headers) and httpAuthKey != None:
-        return web.json_response({'status': False, 'comment': 'AuthKey header is required.'})
-    if httpAuthKey == None or (request.headers['AuthKey'] == httpAuthKey):
-        requesttype = request.match_info['type']
-        try:
-            requestdata = await request.json()
-        except json.decoder.JSONDecodeError:
-            requestdata = None
-        req = simpleobsws.Request(requesttype, requestdata)
+async def request_callback(request, emit):
+    if not ws or not ws.is_identified():
+        return fail_response('obs-websocket is not connected.')
+    authOk, comment = validate_request(request)
+    if not authOk:
+        return fail_response(comment)
+
+    requestType = request.match_info.get('requestType')
+    if not requestType:
+        return fail_response('Your path is missing a request type.')
+    requestData = await get_json(request)
+    req = simpleobsws.Request(requestType, requestData)
+
+    logging.info('Performing request for request type `{}` | Emit: {} | Client IP: {}'.format(requestType, emit, request.remote))
+    logging.debug('Request data:\n{}'.format(requestData))
+
+    if emit:
         await ws.emit(req)
-        return web.json_response({'status': True})
-    else:
-        return web.json_response({'status': False, 'comment': 'Bad AuthKey'})
+        return web.json_response({'result': True})
 
-async def handle_call_request(request):
-    """Handler function for all call-based HTTP requests."""
-    if ('AuthKey' not in request.headers) and httpAuthKey != None:
-        return web.json_response({'status': False, 'comment': 'AuthKey header is required.'})
-    if httpAuthKey == None or (request.headers['AuthKey'] == httpAuthKey):
-        requesttype = request.match_info['type']
-        try:
-            requestdata = await request.json()
-        except json.decoder.JSONDecodeError:
-            if (await request.text()) == '':
-                requestdata = None
-        try:
-            req = simpleobsws.Request(requesttype, requestdata)
-            ret = await ws.call(req)
-            responsedata = {'status': True, 'response': response_to_object(ret)}
-        except simpleobsws.MessageTimeout:
-            responsedata = {'status': False, 'comment': 'The obs-websocket request timed out.'}
-        return web.json_response(responsedata)
-    else:
-        return web.json_response({'status': False, 'comment': 'Bad AuthKey'})
+    try:
+        ret = await ws.call(req)
+    except simpleobsws.MessageTimeout:
+        return fail_response('The obs-websocket request timed out.')
+    responseData = {'result': True, 'requestResult': response_to_object(ret)}
+    return web.json_response(responseData)
 
-async def init_ws():
+async def call_request_callback(request):
+    return await request_callback(request, False)
+
+async def emit_request_callback(request):
+    return await request_callback(request, True)
+
+async def init():
+    logging.info('Connecting to obs-websocket...')
     await ws.connect()
     if not await ws.wait_until_identified():
-        print('Identification with obs-websocket timed out. Could it be using 4.x?')
+        logging.error('Identification with obs-websocket timed out. Could it be using 4.x?')
         return False
+    logging.info('Connected to obs-websocket.')
     return True
 
-app = web.Application()
-app.add_routes([web.post('/emit/{type}', handle_emit_request), web.post('/call/{type}', handle_call_request)])
-statusmessage('Connecting to obs-websocket')
-if not loop.run_until_complete(init_ws()):
-    os._exit(1)
-print('[Connected.]')
-try:
-    web.run_app(app, host=httpAddress, port=httpPort)
-except KeyboardInterrupt:
-    print('Shutting down...')
+async def shutdown(app):
+    logging.info('Shutting down...')
+    if ws.is_identified():
+        logging.info('Disconnecting from obs-websocket...')
+        await ws.disconnect()
+        logging.info('Disconnected from obs-websocket.')
+    else:
+        logging.info('Not connected to obs-websocket, not disconnecting.')
+
+if __name__ == '__main__':
+    config = ConfigParser()
+    config.read('config.ini')
+
+    # Command line args take priority, with fallback to config.ini, and further fallback to defaults.
+    parser = argparse.ArgumentParser(description='A Python-based program that provides HTTP endpoints for obs-websocket')
+    parser.add_argument('--http_bind_addres', dest='http_bind_addres', default=config.get('http', 'bind_to_address', fallback='0.0.0.0'))
+    parser.add_argument('--http_bind_port', dest='http_bind_port', type=int, default=config.getint('http', 'bind_to_port', fallback=4445))
+    parser.add_argument('--http_auth_key', dest='http_auth_key', default=config.get('http', 'authentication_key', fallback=''))
+    parser.add_argument('--ws_url', dest='ws_url', default=config.get('obsws', 'ws_url', fallback='ws://127.0.0.1:4444'))
+    parser.add_argument('--ws_password', dest='ws_password', default=config.get('obsws', 'ws_password', fallback=''))
+    args = parser.parse_args()
+
+    httpAddress = args.http_bind_addres
+    httpPort = args.http_bind_port
+    httpAuthKey = args.http_auth_key
+    wsUrl = args.ws_url
+    wsPassword = args.ws_password
+
+    if httpAuthKey:
+        logging.info('HTTP server will start with AuthKey set to `{}`'.format(httpAuthKey))
+    else:
+        logging.info('HTTP server will start without authentication.')
+        httpAuthKey = None
+
+    ws = simpleobsws.WebSocketClient(url=wsUrl, password=wsPassword)
+
+    if not loop.run_until_complete(init()):
+        os._exit(1)
+
+    app.add_routes([web.post('/call/{requestType}', call_request_callback), web.post('/emit/{requestType}', emit_request_callback)])
+    app.on_cleanup.append(shutdown)
+
+    web.run_app(app, host=httpAddress, port=httpPort, loop=loop)
